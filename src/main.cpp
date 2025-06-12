0c1e60b5c4a3ec310d383588e16eaec5344667be
onon1101 <mingzhuguly@gmail.com>
2025-06-12 14:28:10 +0800

    wtf

--- src/main.cpp
+++ src/main.cpp
@@ -14,10 +14,49 @@ int main() {
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
 
+    // 🔽 顯示可用的 Vulkan 擴充功能
     uint32_t extensionCount = 0;
     vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+    std::vector<VkExtensionProperties> extensions(extensionCount);
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
 
-    std::cout << extensionCount << " extensions supported\n";
+    std::cout << "Available Vulkan extensions:\n";
+    for (const auto& ext : extensions) {
+        std::cout << '\t' << ext.extensionName << '\n';
+    }
+    
+    VkInstance instance;
+
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Vulkan Example";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions;
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+    std::vector<const char*> requiredExtensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+    requiredExtensions.push_back("VK_KHR_portability_enumeration");
+
+    createInfo.enabledExtensionCount = static_cast<uint32_t>(requiredExtensions.size());
+    createInfo.ppEnabledExtensionNames = requiredExtensions.data();
+
+    // ✅ 必須加這行讓 portability extension 有效
+    createInfo.flags = VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+        std::cerr << "Failed to create Vulkan instance" << std::endl;
+        return -1;
+    } else {
+        std::cout << "Vulkan instance created successfully" << std::endl;
+    }
 
     glm::mat4 matrix;
     glm::vec4 vec;
@@ -27,6 +66,7 @@ int main() {
         glfwPollEvents();
     }
 
+    vkDestroyInstance(instance, nullptr);
     glfwDestroyWindow(window);
 
     glfwTerminate();
