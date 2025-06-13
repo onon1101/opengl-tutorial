fd27c48d47bb6b16b3e8f21bc715151515af29f6
onon1101 <mingzhuguly@gmail.com>
2025-06-14 07:58:48 +0800

    square texture

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,9 @@
 #include <sstream>
 #include <string>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 #include "Shader.hpp"
 
 void
@@ -73,34 +76,85 @@ main(int, char **)
 	Shader shaderProgram(SOURCE_DIR "shader.vert", SOURCE_DIR "shader.frag");
 
 	/*
+		load image and generating texture
+	*/
+	int width, height, nrChannels;
+	unsigned char *data =
+		stbi_load(ASSETS_DIR "container.jpg", &width, &height, &nrChannels, 0);
+
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	// config
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+					GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+					 GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+
+	stbi_image_free(data);
+
+	/*
 		設定 vertices buffer and vertex attribute。
 	*/
-	float vertices[] = {// position                         // color
-						0.0f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f,
-						0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
-						-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f};
+	// clang-format off
+	float vertices[] = {
+    // positions          // colors           // texture coords
+     0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+     0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+    -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+    -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
+	};
 
+	unsigned int indices[] = {
+		0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+	};
+
+	// clang-format on
 	unsigned int VBO; // or GLuint. declare the value of the buffer id.
 	unsigned int VAO; // declare vertex attribute object.
-	// unsigned int EBO;
+	unsigned int EBO;
 	glGenVertexArrays(1, &VAO); // just like vbo
 	glGenBuffers(1, &VBO);		// generate vbo buffer id via opengl.
-	// glGenBuffers(1, &EBO);
+	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO); // binding
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO); // binding VBO to GL_ARRAY_BUFFER.
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+				 GL_STATIC_DRAW);
+
 	/*
 		linking vertex attr.
 	*/
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
 						  (void *)0);
 	glEnableVertexAttribArray(0);
 
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
 						  (void *)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
+
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+						  (void *)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+	glUniform1i(glGetUniformLocation(shaderProgram.ID, "ourTexture"), 0);
 
 	/*
 		開始 loop to rendering
@@ -119,7 +173,7 @@ main(int, char **)
 
 		// uniform
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		// checking
 		glfwSwapBuffers(window);
