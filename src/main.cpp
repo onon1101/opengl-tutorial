46c504f719f6520ff6439201e7f9de8b9ef5492f
onon1101 <mingzhuguly@gmail.com>
2025-06-26 01:00:14 +0800

    3D box that can't not overlayer

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 */
 // clang-format off
 #include "glm/ext/matrix_clip_space.hpp"
+#include "glm/ext/quaternion_transform.hpp"
 #include "glm/fwd.hpp"
 #include "glm/trigonometric.hpp"
 #include <glad/glad.h>
@@ -68,6 +69,8 @@ main(int, char **)
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
+
+	// glEnable(GL_DEPTH_TEST);
 
 	/*
 		設定 viewport。
@@ -144,52 +147,84 @@ main(int, char **)
 	*/
 	// clang-format off
 	float vertices[] = {
-    // positions          // colors           // texture coords
-     0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
-     0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
-    -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
-    -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
-	};
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
 
-	unsigned int indices[] = {
-		0, 1, 3, // first triangle
-        1, 2, 3  // second triangle
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 	};
 
 	// clang-format on
 	unsigned int VBO; // or GLuint. declare the value of the buffer id.
 	unsigned int VAO; // declare vertex attribute object.
-	unsigned int EBO;
+	// unsigned int EBO;
 	glGenVertexArrays(1, &VAO); // just like vbo
 	glGenBuffers(1, &VBO);		// generate vbo buffer id via opengl.
-	glGenBuffers(1, &EBO);
+	// glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO); // binding
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO); // binding VBO to GL_ARRAY_BUFFER.
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
-				 GL_STATIC_DRAW);
+	// glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	// glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cubePositions), cubePositions,
+	// 			 GL_STATIC_DRAW);
 
 	/*
 		linking vertex attr.
 	*/
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
 						  (void *)0);
 	glEnableVertexAttribArray(0);
 
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
 						  (void *)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
 
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
-						  (void *)(6 * sizeof(float)));
-	glEnableVertexAttribArray(2);
+	// glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+	// 					  (void *)(6 * sizeof(float)));
+	// glEnableVertexAttribArray(2);
 
 	shaderProgram.use();
-	glUniform1i(glGetUniformLocation(shaderProgram.ID, "texture0"), 0);
+	// glUniform1i(glGetUniformLocation(shaderProgram.ID, "texture0"), 0);
+	shaderProgram.setInt("texture0", 0);
 	shaderProgram.setInt("texture1", 1);
 
 	/*
@@ -202,7 +237,7 @@ main(int, char **)
 
 		// rendering
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // background
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// binding texture
 		glActiveTexture(GL_TEXTURE0);
@@ -210,29 +245,27 @@ main(int, char **)
 		glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D, texture1);
 
-		// activate shader 
+		// activate shader
 		shaderProgram.use();
 
 		// create coordinate system
-		glm::mat4 model = glm::mat4(1.0f); // model matrix: object space -> world space
-		model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
- 
-		glm::mat4 view = glm::mat4(1.0f); // view matrix: world space -> view space.
+		glm::mat4 model = glm::mat4(1.0f);
+		model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, -1.0f, 0.0f));
+		
+		glm::mat4 view =
+			glm::mat4(1.0f); // view matrix: world space -> view space.
 		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 
-		glm::mat4 projection = glm::mat4(1.0f); // projection matrix: view space -> clip space.
+		glm::mat4 projection =
+			glm::mat4(1.0f); // projection matrix: view space -> clip space.
 		projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
-
-		GLuint modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-		GLuint viewLoc = glGetUniformLocation(shaderProgram.ID, "view");
-		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
-
 		shaderProgram.setMat4("projection", projection);
+		shaderProgram.setMat4("view", view);
+		shaderProgram.setMat4("model", model);
 
-		// uniform
+		// renderer
 		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 
 		// checking
 		glfwSwapBuffers(window);
