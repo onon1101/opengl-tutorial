d59d08352586ac4cf07d058a44d77439e43de6f4
onon1101 <mingzhuguly@gmail.com>
2025-06-26 12:09:24 +0800

    3D cube with deep information

--- src/main.cpp
+++ src/main.cpp
@@ -70,7 +70,7 @@ main(int, char **)
 		return -1;
 	}
 
-	// glEnable(GL_DEPTH_TEST);
+	glEnable(GL_DEPTH_TEST);
 
 	/*
 		設定 viewport。
@@ -250,7 +250,7 @@ main(int, char **)
 
 		// create coordinate system
 		glm::mat4 model = glm::mat4(1.0f);
-		model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, -1.0f, 0.0f));
+		model = glm::rotate(model, static_cast<float>(glfwGetTime()) * glm::radians(-55.0f), glm::vec3(1.0f, -1.0f, 0.0f));
 		
 		glm::mat4 view =
 			glm::mat4(1.0f); // view matrix: world space -> view space.
